94eb53c56006f6dd444baf15e351f5cc31addc30
Toukir Ahammed <ahmdtoukir@gmail.com>
2023-03-06 11:40:46 +0600

    changed mongo url

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,10 +1,10 @@
 // @ts-nocheck
 export class Constants {
   static PORT = process.env.PORT || 8000;
-  static APP_NAME = process.env.APP_NAME || "DENJI";
+  static APP_NAME = process.env.APP_NAME || "Ronin";
   static EXPIRES_IN = process.env.EXPIRES_IN || "3000s";
   static DATABASE =
     process.env.DATABASE ||
-    "mongodb+srv://rafin:CHoOPq3tTfzNx1DM@cluster0.khgm0kt.mongodb.net/test";
+    "mongodb+srv://ahmdtoukir:Ixk85C7FkU0FTfsp@servertestcluster.5z0e22z.mongodb.net/?retryWrites=true&w=majority";
   static SECRET = process.env.SECRET || "secretKey";
 }
